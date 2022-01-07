e9acc4947e802918feffa1a4b9a2d428d377779f
Takács Balázs <tbalazs.levente@gmail.com>
2022-01-07 19:43:55 +0100

    Felhasznalo osztály létrehozása

---
+++ src/main/java/hu/doggo/doggo_admininterface/Felhasznalo.java
@@ -0,0 +1,45 @@
+package hu.doggo.doggo_admininterface;
+
+import java.util.Date;
+
+public class Felhasznalo {
+    private int id;
+    private String felhasznalonev;
+    private String email;
+    private Date regisztracio_datum;
+    private Boolean admin;
+    private Boolean tiltva;
+
+    public Felhasznalo(int id, String felhasznalonev, String email, Date regisztracio_datum, Boolean admin, Boolean tiltva) {
+        this.id = id;
+        this.felhasznalonev = felhasznalonev;
+        this.email = email;
+        this.regisztracio_datum = regisztracio_datum;
+        this.admin = admin;
+        this.tiltva = tiltva;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFelhasznalonev() {
+        return felhasznalonev;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getRegisztracio_datum() {
+        return regisztracio_datum;
+    }
+
+    public Boolean getAdmin() {
+        return admin;
+    }
+
+    public Boolean getTiltva() {
+        return tiltva;
+    }
+}
--- src/main/java/hu/doggo/doggo_admininterface/FelhasznalokController.java
+++ src/main/java/hu/doggo/doggo_admininterface/FelhasznalokController.java
@@ -1,14 +1,24 @@
 package hu.doggo.doggo_admininterface;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.util.Date;
 
 public class FelhasznalokController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TableColumn<Felhasznalo, Date> regDateCol;
+    @FXML
+    private TableColumn<Felhasznalo, Boolean> tiltvaCol;
+    @FXML
+    private TableColumn<Felhasznalo, String> felhnevCol;
+    @FXML
+    private TableColumn<Felhasznalo, String> emailCol;
+    @FXML
+    private TableView<Felhasznalo> felhasznalokTableView;
+    @FXML
+    private TableColumn<Felhasznalo, Boolean> adminCol;
+
 }
