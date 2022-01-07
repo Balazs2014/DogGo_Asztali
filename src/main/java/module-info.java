b99342d5ffa129751b7ad755f8b7407167e3e512
Takács Balázs <tbalazs.levente@gmail.com>
2022-01-07 16:12:42 +0100

    Projekt létrehozása

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module hu.doggo.doggo_admininterface {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens hu.doggo.doggo_admininterface to javafx.fxml;
+    exports hu.doggo.doggo_admininterface;
+}
