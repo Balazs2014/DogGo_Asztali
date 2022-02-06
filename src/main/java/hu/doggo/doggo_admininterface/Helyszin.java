f78cea05dfb65c0054bab51bee9ec593fc00906f
Takács Balázs <tbalazs.levente@gmail.com>
2022-02-06 14:20:11 +0100

    HelyszinController, API

---
+++ src/main/java/hu/doggo/doggo_admininterface/Helyszin.java
@@ -0,0 +1,43 @@
+package hu.doggo.doggo_admininterface;
+
+public class Helyszin {
+    private int id;
+    private String name;
+    private double lat;
+    private double lng;
+
+    public Helyszin(int id, String name, double lat, double lng) {
+        this.id = id;
+        this.name = name;
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+}
--- src/main/java/hu/doggo/doggo_admininterface/controllers/HelyszinekController.java
+++ src/main/java/hu/doggo/doggo_admininterface/controllers/HelyszinekController.java
@@ -1,5 +1,59 @@
 package hu.doggo.doggo_admininterface.controllers;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import hu.doggo.doggo_admininterface.Felhasznalo;
+import hu.doggo.doggo_admininterface.Helyszin;
+import hu.doggo.doggo_admininterface.RequestHandler;
+import hu.doggo.doggo_admininterface.Response;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
 public class HelyszinekController {
 
+    @FXML
+    private TableColumn<Helyszin, Double> lngCol;
+    @FXML
+    private TableView<Helyszin> helyszinekTableView;
+    @FXML
+    private TableColumn<Helyszin, String> nameCol;
+    @FXML
+    private TableColumn<Helyszin, Double> latCol;
+
+    public void initialize() {
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        latCol.setCellValueFactory(new PropertyValueFactory<>("lat"));
+        lngCol.setCellValueFactory(new PropertyValueFactory<>("lng"));
+
+        helyszinListaFeltoltes();
+
+    }
+
+    private void helyszinListaFeltoltes() {
+        try {
+            Response response = RequestHandler.get("http://127.0.0.1:8000/api/locations");
+            String json = response.getContent();
+            if(response.getResponseCode() >= 400) {
+                System.out.println(json);
+                return;
+            }
+
+            Gson jsonConverter = new Gson();
+            Type type = new TypeToken<List<Helyszin>>(){}.getType();
+            List<Helyszin> helyszinLista = jsonConverter.fromJson(json, type);
+            helyszinekTableView.getItems().clear();
+            for (Helyszin helyszin : helyszinLista) {
+                helyszinekTableView.getItems().add(helyszin);
+            }
+
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
 }
